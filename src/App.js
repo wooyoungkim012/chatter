f62eb234143130f26e2822361d8ebfab0b1bc2ef
Woo Young Kim <kimwy012@gmail.com>
2022-01-24 23:01:59 -0800

    basic structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'>
+        <div className="logo" />
+        <span className='title'>CHATTER!</span>
+      </header>
+      <footer className='footer'>
+        <input className='text-input' />
+        <button className='send'>↑</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
